ac531ee06e98498929511cd4ee447fc7b569f193
박상수 <qkzn326@gmail.com>
2024-03-22 17:40:36 +0900

    20240322 커밋

--- src/com/javaex/practice/Ex08.java
+++ src/com/javaex/practice/Ex08.java
@@ -6,13 +6,27 @@ public class Ex08 {
 
 	public static void main(String[] args) {
 		// 미니로또 만들기 1~45
+		
+		int[] num = new int[6];
+		int count = 0;
 
-		// 랜덤 숫자 1~45까지 생성
-		
-		// 중복체크
-		
-		// 숫자 6자리 출력
-		
-		
+		while (count < 6) {
+		    int randomNumber = (int)(Math.random()*45) + 1; // 1~45까지 랜덤 숫자 생성
+		    boolean duplicate = false; // 중복체크 초기화
+		    for (int i=0; i<count; i++) {
+		        if(num[i] == randomNumber) { // 중복 체크
+		            duplicate = true;
+		            break;
+		        }
+		    }
+		    if(!duplicate) { // 중복이 아니면 배열에 추가
+		        num[count] = randomNumber;
+		        count++;
+		    }
+		}
+
+		for (int number : num) {
+		    System.out.print(number + " ");
+		    }
+		}
 	}
-}
